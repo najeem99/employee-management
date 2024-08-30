0c0ca2895e2fc0df6ed3e8013b16f073462ed2d7
najeem <najeemfasil1@gmail.com>
2024-08-30 21:02:06 +0400

    pushing missed files and changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
     {
         path: "employee-details",
         title: "Employee Details",
         component: EmployeeDetailsComponent,
+       },
+    {
+        path: "about",
+        title: "About",
+        component: AboutComponent,
        },
        {
         path: "",
