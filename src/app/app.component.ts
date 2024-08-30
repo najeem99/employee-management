6120b4c0318696b6aa19c6b9d79fe2b97d6276c4
najeem <najeemfasil1@gmail.com>
2024-08-30 19:45:42 +0400

    css changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatSidenavModule,MatListModule],
+  imports: [RouterOutlet,MatSidenavModule,MatListModule,MatToolbarModule,MatIconModule,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
